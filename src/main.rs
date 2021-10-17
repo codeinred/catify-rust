ee1c75ee9aecc0fbbc8ec031eea88ed97a682395
Alecto Irene Perez <perez.cs@pm.me>
2021-10-17 01:13:58 -0600

    Implemented guessing game 💕💅
    Signed-off-by: Alecto Irene Perez <perez.cs@pm.me>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guessing game");
+
+    println!("Guess the number:");
+
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 }
