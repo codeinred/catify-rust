b371a3785668c57db78a44abd8eeeba6f7926650
Alecto Irene Perez <perez.cs@pm.me>
2021-10-17 01:31:48 -0600

    Implemented fibonacci 😌🐈
    Signed-off-by: Alecto Irene Perez <perez.cs@pm.me>

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 use std::io;
 
-fn main() {
-    println!("Guessing game");
+fn fib(n: u64, first: u64, second: u64) -> u64 {
+    if n == 0 {
+        first
+    } else {
+        fib(n - 1, second, first + second)
+    }
+}
 
-    println!("Guess the number:");
+fn main() {
 
     let mut guess = String::new();
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+    let n = guess.trim().parse::<u64>().expect("Unable to parse number");
+    println!("fib({}) = {}", n, fib(n, 1, 1));
 }
