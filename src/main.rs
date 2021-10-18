2b358b8afcc1248db9f0eff408fa7eaa10e5a08b
Alecto Irene Perez <perez.cs@pm.me>
2021-10-18 02:40:39 -0600

    Formatting 💅🐈
    Signed-off-by: Alecto Irene Perez <perez.cs@pm.me>

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,26 @@
 use std::fs::File;
 use std::io::prelude::*;
 
-fn read_all(file : &mut File) -> String {
+fn read_all(file: &mut File) -> String {
     let mut contents = String::new();
-    file.read_to_string(&mut contents).expect("Unable to read contents of file");
+    file.read_to_string(&mut contents)
+        .expect("Unable to read contents of file");
     contents
 }
-fn catify(file : &String) {
+fn catify(file: &String) {
     let mut file = File::open(file).expect("Unable to open the file");
     let contents = read_all(&mut file);
     println!("Contents of file: {}", contents);
-
 }
 fn main() {
-    let args : Vec<String> = std::env::args().collect();
-   
+    let args: Vec<String> = std::env::args().collect();
+
     if args.len() == 1 {
         println!("Usage: {} <filename>", args[0]);
-        return 
+        return;
     }
 
     let filename = &args[1];
-    
-    catify(&filename);   
+
+    catify(&filename);
 }
